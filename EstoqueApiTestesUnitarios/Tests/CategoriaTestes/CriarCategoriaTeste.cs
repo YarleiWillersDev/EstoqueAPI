e3941d63d124b05d35807453fff9f0ba9d0f22e6
YarleiWillersDev <yarleiwillers@gmail.com>
2026-04-16 21:40:28 -0300

    Cria projeto XUnit para testes unitários da API principal

---
+++ EstoqueApiTestesUnitarios/Tests/CategoriaTestes/CriarCategoriaTeste.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstoqueApiTestesUnitarios.Tests.CategoriaTestes
+{
+    public class CriarCategoriaTeste
+    {
+    
+        [Fact]
+        public void Deve_Retornar_Sucesso_Ao_Criar_Categoria_Com_Dados_Corretos()
+        {
+            
+        }
+
+    }
+}
---
+++ EstoqueApiTestesUnitarios/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace EstoqueApiTestesUnitarios;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
