9027ceee89940b533a0b89663d8b0600052e7bbc
YarleiWillersDev <yarlei.willers@gmail.com>
2026-04-17 10:43:04 -0300

    Criação de teste unitário para validar método de criação de categoria.

--- EstoqueApiTestesUnitarios/Tests/CategoriaTestes/CriarCategoriaTeste.cs
+++ EstoqueApiTestesUnitarios/Tests/CategoriaTestes/CriarCategoriaTeste.cs
@@ -2,17 +2,51 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EstoqueApi.Context;
+using EstoqueApi.DTOs;
+using EstoqueApi.Service;
+using Microsoft.EntityFrameworkCore;
 
 namespace EstoqueApiTestesUnitarios.Tests.CategoriaTestes
 {
     public class CriarCategoriaTeste
     {
-    
-        [Fact]
-        public void Deve_Retornar_Sucesso_Ao_Criar_Categoria_Com_Dados_Corretos()
+
+        private AppDbContext CriarBancoEmMemoria()
         {
-            
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            return new AppDbContext(options);
         }
 
+        [Fact]
+        public async Task Deve_Retornar_Sucesso_Ao_Criar_Categoria_Com_Dados_Corretos()
+        {
+            
+            //Arrange
+            var context = CriarBancoEmMemoria();
+            var service = new CategoriaService(context);
+
+            var request = new CategoriaRequest
+            {
+                Nome = "Bebidas"
+            };
+
+            // Act
+            var result = await service.CreateAsync(request);
+
+            // Assert (resultado)
+            Assert.NotNull(result); // Valida se o resultado não é null
+            Assert.Equal("Bebidas", result.Nome); // Valida se o campo Nome é igual ao request
+            Assert.True(result.Id > 0); // Validad se o ID é maior ou igual a 0
+            
+            // Assert (Dados no Banco)
+            var categoriaSalvaNoBanco = await context.Categorias.FirstOrDefaultAsync();
+
+            Assert.NotNull(categoriaSalvaNoBanco);
+            Assert.Equal("Bebidas", categoriaSalvaNoBanco.Nome);
+        }
     }
 }
